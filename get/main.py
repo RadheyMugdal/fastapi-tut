3a2009e82d66cc65b0a6efad7eb11aef8a5fecbc
radheymugdal <mugdalradhey@gmail.com>
2026-06-11 23:08:05 +0530

    Day 3 of learning query & path params in fastapi

--- get/main.py
+++ get/main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Request
+from mockData import products
 
 app=FastAPI()
 
 @app.get('/')
 def home():
     return "Welcome to fastapi"
+
+
+# How to have path and qurey params in faat api route
+
+@app.get('/products')
+def getProductions():
+    return products
+
+
+@app.get('/products/{productId}')
+def getProductById(productId:int):
+    # if product available with id return product el    se return error
+    for i in products:
+        if i.get('id')==productId:
+            return i
+
+    return {
+        "error":f"product not found for this id {productId}"
+    }
+
+# query param in fastapi 
+@app.get("/greet")
+def greet_user(name:str | None= None):
+    return "Hello, how are you? {name}".format(name=name)
+
+# Or we can  also do it below way 
+
+@app.get("/say-hello")
+def greet_user(request:Request):
+    return "Hello, how are you? {name}".format(name=request.query_params.get("name"))
---
+++ get/mockData.py
@@ -0,0 +1,23 @@
+products=[
+    {   
+        "id":1,
+        "title":"laptop",
+        "price": 50000,
+        "description": "This is a laptop",
+        "count":10
+    },
+    {
+        "id":2,
+        "title":"mobile",
+        "price": 20000,
+        "description": "This is a mobile",
+        "count":20
+    },
+    {
+        "id":3,
+        "title":"tablet",
+        "price": 30000,
+        "description": "This is a tablet",
+        "count":30
+    }
+]
